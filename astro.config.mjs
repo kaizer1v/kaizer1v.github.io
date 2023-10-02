9103317789edf129a2ab7b3fad00606b904d36ee
Vivek S <kaizer1v@gmail.com>
2023-10-02 22:29:30 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,23 @@
 import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'vivekipedia.in'
-  // base: '/my-repo',
+  site: 'vivekipedia.in',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
+    rehypePlugins: [
+      [
+        'rehype-external-links',
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
 })
