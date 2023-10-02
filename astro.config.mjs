67666d4a183f6376e1de7ae2afcc2b5ad951f34d
kaizer1v <kaizer1v@gmail.com>
2023-10-02 20:32:38 +0530

    deploy, build astro to github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,6 @@
 import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord',
-    },
-    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
-    rehypePlugins: [
-      [
-        'rehype-external-links',
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
+  site: 'vivekipedia.in'
+  // base: '/my-repo',
 })
