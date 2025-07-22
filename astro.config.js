68664cfac28517979b798c800b9c4580edc76912
kaizer1v <kaizer1v@gmail.com>
2025-07-22 17:52:11 +0530

    removed svelte dependency

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
+import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kaizer1v.github.io',
-  integrations: [mdx(), svelte()],
+  // site: 'https://vivekipedia.in',
+  // base: '/',
+  // trailingSlash: 'always',
+  integrations: [mdx(), react()],
   markdown: {
     shikiConfig: {
       theme: 'nord',
